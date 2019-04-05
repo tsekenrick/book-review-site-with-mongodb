f9d1eb83b6bcb98f947ebeb1fca1ec404a402513
tsekenrick <kt1556@nyu.edu>
2019-04-05 17:55:17 -0400

    pretty much all boiler-plate setup complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,55 @@
 const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
+const mongoSanitize = require('mongo-sanitize');
+const path = require('path');
 const app = express();
+require('./db');
 
-// your code goes here!
+const sessionOptions = { 
+	secret: 'secret for signing session id', 
+	saveUninitialized: false, 
+	resave: false 
+};
+
+app.set('view engine', 'hbs');
+app.use(session(sessionOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+// another app.use later to handle 404s:
+
+// models and slug definition
+const Book = mongoose.model('Book');
+const Review = mongoose.model('Review');
+Book.plugin(URLSlugs('title author'));
+
+app.get('/', (req, res) => {
+    res.redirect('/books');
+});
+
+app.get('/books', (req, res) => {
+    res.render('books');
+});
+
+app.get('/books-new', (req, res) => {
+
+});
+
+app.post('/books-new', (req, res) => {
+
+});
+
+app.get('/books/:slug', (req, res) => {
+
+});
+
+app.post('/books/:slug/comments', (req, res) => {
+
+});
+
+app.get('books/mine', (req, res) => {
+
+});
 
 app.listen(3000);
--- src/db.js
+++ src/db.js
@@ -1,1 +1,21 @@
 // db.js
+const mongoose = require('mongoose');
+
+// schema
+const ReviewSchema = new mongoose.Schema({
+    rating: {type: Number, required: true},
+    name: {type: String},
+    text: {type: String, required: true}
+});
+
+const BookSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    isbn: {type: String, required: true},
+    reviews: [ReviewSchema]
+});
+
+mongoose.model("Book", BookSchema);
+mongoose.model("Review", ReviewSchema);
+
+mongoose.connect('mongodb://localhost/hw05');
